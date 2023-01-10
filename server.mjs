dc20003c6bc987da688996a75294a690c465096f
duynghia-123 <duynghia22302@gmail.com>
2023-01-11 06:10:58 +0700

    Connect Database to Mongoose

--- server.mjs
+++ server.mjs
@@ -5,7 +5,9 @@ import { ApolloServer } from "@apollo/server";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import { expressMiddleware } from "@apollo/server/express4";
 import cors from "cors";
+import mongoose from "mongoose";
 import fakeData from "./fakeData/index.js";
+import "dotenv/config";
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -14,8 +16,14 @@ const typeDefs = `#graphql
   type Folder {
     id: String,
     name: String,
-    createAt: String,
+    createdAt: String,
     author: Author
+    notes: [Note]
+  }
+
+  type Note {
+    id: String,
+    content: String
   }
 
   type Author {
@@ -24,7 +32,9 @@ const typeDefs = `#graphql
   }
 
   type Query {
-    folders: [Folder]
+    folders: [Folder],
+    folder(folderId: String): Folder,
+    note(noteId: String): Note
   }`;
 
 const resolvers = {
@@ -32,19 +42,32 @@ const resolvers = {
     folders: () => {
       return fakeData.folders;
     },
+    folder: (parent, args) => {
+      const folderId = args.folderId;
+      console.log({ folderId });
+      return fakeData.folders.find((folder) => folder.id === folderId);
+    },
+    note: (parent, args) => {
+      const noteId = args.noteId;
+      return fakeData.notes.find((note) => note.id === noteId);
+    },
   },
   Folder: {
     author: (parent, args) => {
       console.log({ parent, args });
       const authorId = parent.authorId;
       return fakeData.authors.find((author) => author.id === authorId);
-      // return {
-      //   id: "123",
-      //   name: "HoleTex",
-      // };
+    },
+    notes: (parent, args) => {
+      console.log({ parent });
+      return fakeData.notes.filter((note) => note.folderId === parent.id);
     },
   },
 };
+
+// connect Database
+const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.tksacev.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 4000;
 
 const server = new ApolloServer({
   typeDefs,
@@ -56,5 +79,15 @@ await server.start();
 
 app.use(cors(), bodyParser.json(), expressMiddleware(server));
 
-await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
-console.log("🚀 Server ready at http://localhost:4000");
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    console.log("Connected to DB");
+
+    await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+    console.log("🚀 Server ready at http://localhost:4000");
+  });
